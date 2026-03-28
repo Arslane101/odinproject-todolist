4e0c9c8d61d0d63f299431a3e1ccbe9a1c34286d
Arslane101 <rahalabdelhamid@gmail.com>
2026-03-28 15:16:01 +0100

    Added the option to switch projects and to navigate between projects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-console.log("hello")
 const submit = document.querySelector("#submit-form")
 const dialog =document.querySelector("#task-form")
 const form = document.querySelector("#form-task")
+const data = document.querySelector("#combo-input")
+import {isToday} from "date-fns"
 import colors  from "./index2.js"
-
 class Todos {
     constructor(title, description , dueDate, priority){
         this.description = description
@@ -23,6 +23,9 @@ class Todos {
         setProject(project) {
             this.project = project
         }
+        getDate() {
+            return `${this.dueDate}`
+        }
 }
 function createLayout(task){
     let today = document.querySelector("#today")
@@ -33,7 +36,7 @@ function createLayout(task){
     taskspace.style.paddingBottom = "5px"
     taskspace.style.gridTemplateColumns = ".5fr 2fr 1fr"
     taskspace.style.alignItems = "center"
-
+    taskspace.style.transition = "all 0.5s ease"
     let titledesc = document.createElement("div")
     titledesc.style.display = "flex"
     titledesc.style.flexDirection = "column"
@@ -46,6 +49,14 @@ function createLayout(task){
         if(checkbox.checked) {
             styles.insertRule(":checked { background-color:"+ colors[4-parseInt(task.priority.at(1))] + "}")
             checkbox.classList.add("checked")
+            taskspace.style.opacity = "0"
+            taskspace.style.transform = "translateX(50px)"
+            taskspace.style.maxHeight = "0"
+            taskspace.style.pointerEvents = "none"
+            taskspace.addEventListener('transitionend',() => {
+                taskspace.remove()
+            },{once : true})
+            
         }
             else {
                 checkbox.classList.remove("checked")
@@ -61,6 +72,8 @@ function createLayout(task){
 
     let title = document.createElement("p")
     title.style.marginBottom = "4px"
+    title.style.fontSize = "25px"
+    title.style.fontWeight = "bold"
     title.textContent = task.getTitle()
 
     
@@ -68,30 +81,39 @@ function createLayout(task){
     description.style.marginTop = "0"
     description.textContent = task.getDescription()
 
-    let dueDate = document.createElement("p")
-    dueDate.textContent = tasklist.dueDate
-
     today.append(taskspace)
     taskspace.append(checkbox)
     taskspace.append(titledesc)
     titledesc.append(title)
     titledesc.append(description)
-    taskspace.append(dueDate)
+
+    if(isToday(task.dueDate) === false){
+        let dueDate = document.createElement("p")
+        dueDate.textContent = task.dueDate
+        taskspace.appendChild(dueDate)
+
+
+    }
+
+    
 
 }
 let tasklist = []
 function submittask(event){
     event.preventDefault()
     let formData = new FormData(form,submit)
+    
     dialog.close()
     let array = Array.from(formData.values())
+    console.log(array)
     let task = new Todos(...array)
+    if(data.value !== "default") task.setProject(data.value)
     tasklist.push(task)
     console.log(task)
-    createLayout(task)
+    if(isToday(task.getDate())=== true) createLayout(task)
     
     
 
 }
 
-export  {Todos,submittask}
+export  {Todos,submittask,tasklist,createLayout}
--- src/index2.js
+++ src/index2.js
@@ -1,12 +1,10 @@
-import {Todos, submittask} from "./index.js"
+import {tasklist, submittask,createLayout} from "./index.js"
 import "./style.css"
 import sidebaricon from "./icons/sidebar.svg"
 import addicon from "./icons/plus-circle.svg"
-import flag from "./icons/flag.svg"
 
 let colors = ["#D1453B","#EB8909","#246FE0","black"]
 export default colors
-let todo = new Todos("","","","")
 
 const body = document.querySelector("#container")
 
@@ -60,6 +58,7 @@ addicons.src = addicon
 addicons.style.filter = "invert(39%) sepia(37%) saturate(2327%) hue-rotate(334deg) brightness(94%) contrast(85%)"
 addbutton.className = "sidebar-buttons"
 addicons.style.width = "20px"
+addbutton.style.marginTop = "50px"
 addicons.style.height = "20px"
 addbutton.textContent = "Add task"
 addbutton.style.color = "#DF5141"
@@ -84,7 +83,122 @@ flagicon.style.height = "20px"
 */
 /* Today's tasks*/
 
-
-
 submitbutton.addEventListener("click",submittask)
 
+function clearContent(text){
+    let space = document.getElementById("today")
+    space.innerHTML = ""
+    let heading = document.createElement("h2")
+    heading.textContent = "Project : " + text  
+    heading.style.margin = "0"
+    space.appendChild(heading)
+    let view = tasklist.filter(task => task.project === text)
+    console.log("here")
+    console.log(view)
+    for (const tsk of view){
+        createLayout(tsk)
+    }
+
+}
+
+/*navigate through projects*/
+let projectlist = document.createElement("div")
+projectlist.className = "nav-container" 
+projectlist.style.width = "200px"
+projectlist.style.cursor = "pointer"
+projectlist.style.userSelect = "none"
+let header = document.createElement("div")
+header.id = "projects-toggle"
+header.className = "nav-header"
+header.style.display = "flex"
+header.style.alignItems = "center"
+header.style.padding = "10px"
+header.style.borderRadius = "4px"
+let title = document.createElement("span")
+title.className = "special"
+title.textContent = "My projects"
+
+header.appendChild(title)
+projectlist.appendChild(header)
+
+sidebar.appendChild(projectlist)
+let navlist = document.createElement("ul")
+navlist.className = "nav-list"
+navlist.addEventListener("click", function(event) {
+  // Check if the clicked element (event.target) is an LI
+  if (event.target && event.target.matches("li")) {
+    event.target.classList.add("active");
+    clearContent(event.target.textContent)
+  }
+});
+
+projectlist.append(navlist)
+const toggle = document.getElementById('projects-toggle');
+const container = toggle.parentElement;
+toggle.addEventListener('click', () => {
+  container.classList.toggle('open');
+});
+
+/*Add project and select */
+let combo = document.querySelector("#combo-input")
+let options = document.querySelector("#combo-options")
+let items = []
+function selectItem(val) {
+  combo.value = val;
+  options.style.display = 'none';
+}
+
+function addItem(val) {
+  items.push(val); // Add to our data array
+  selectItem(val); // Select it
+}
+
+function renderList(filter = "") {
+    options.innerHTML = ""
+    const filtered = items.filter(item => item.toLowerCase().includes(filter.toLowerCase()))
+
+    // Show matches : If you didn't filter , it shows everything 
+    filtered.forEach(item => {
+        const option = document.createElement("div")
+        option.className = "option-item"
+        option.textContent = item
+        option.onclick = () => selectItem(item)
+        options.appendChild(option)
+
+    })
+
+    //add button 
+    if( filter && !items.find(i => i.toLowerCase() === filter.toLowerCase())){
+        const addDiv = document.createElement("div")
+        addDiv.className = "new-option"
+        addDiv.textContent = `Add ${filter}`
+        addDiv.onclick = () => {
+            addItem(filter)
+            let li = document.createElement("li")
+            li.textContent = filter
+            li.style.color = "black"
+            li.style.cursor = "pointer"
+            li.style.fontStyle = "italic"
+            navlist.appendChild(li)
+
+
+
+        }
+        options.appendChild(addDiv)
+    }
+    options.style.display = (filtered.length > 0 || filter) ? 'block' : 'none'
+
+}
+combo.addEventListener('input',(e) => renderList(e.target.value))
+
+
+
+for (let prj of items){
+    let li = document.createElement("li")
+    li.textContent = prj
+    li.style.cursor = "pointer"
+    li.style.fontStyle = "italic"
+    navlist.appendChild(li)
+   
+
+}
